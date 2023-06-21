bd240dc19dd99724c40d475a74a219e4a418067b
weilirs <weilirs@umich.edu>
2023-06-21 12:38:03 -0500

    added iframe for execution separation

--- src/index.tsx
+++ src/index.tsx
@@ -1,54 +1,84 @@
-import * as esbuild from 'esbuild-wasm'
-import ReactDOM from 'react-dom'
-import { useState, useEffect, useRef } from 'react'
-import { unpkgPlugin } from './plugins/unpkg-plugin';
-import { fetchPlugin } from './plugins/fetch-plugin';
+import * as esbuild from "esbuild-wasm";
+import ReactDOM from "react-dom";
+import { useState, useEffect, useRef } from "react";
+import { unpkgPlugin } from "./plugins/unpkg-plugin";
+import { fetchPlugin } from "./plugins/fetch-plugin";
 
 const App = () => {
-    const ref = useRef<any>()
-    const [input,setInput] = useState('')
-    const [code, setCode] = useState('')
+  const ref = useRef<any>();
+  const iframe = useRef<any>();
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
 
-    const startService = async () => {
-        ref.current = await esbuild.startService({
-            worker: true,
-            wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
-        })
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
     }
 
-    useEffect(() => {
-        startService()
-    },[])
+    iframe.current.srcdoc = html;
 
-    const onClick = async () => {
-        if(!ref.current) {
-            return
-        }
+    const result = await ref.current.build({
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPlugin(), fetchPlugin(input)],
+      define: {
+        process_env_NODE_ENV: '"production"',
+        global: "window",
+      },
+    });
 
-        const result = await ref.current.build({
-            entryPoints: ['index.js'],
-            bundle: true,
-            write: false,
-            plugins: [unpkgPlugin(), fetchPlugin(input)],
-            define: {
-                'process_env_NODE_ENV': '"production"',
-                global: 'window'
-            }
-        })
+    // console.log(`JS CODE: ${result.outputFiles[0].text}`);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
+  };
 
-        setCode(result.outputFiles[0].text)
-    }
-    return (
-        <div>
-            <textarea value={input} onChange={(e) => setInput(e.target.value)}>
+  const html = `
+    <html>
+        <head></head>
+        <body>
+            <div id="root"></div>
+            <script>
+                window.addEventListener('message', (event) => {
+                    try {
+                        eval(event.data)
+                    } catch (err) {
+                        const root = document.querySelector('#root')
+                        root.innerHTML = '<div style="color: red;"><h4> Runtime Error </h4>' + err + '</div>';
+                        console.log(err)
+                    }
+                },false)
+            </script>
+        </body>
+    </html>
+  `;
 
-            </textarea>
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
 
-            <button onClick={onClick}>
-                Submit
-            </button>
-            <pre>{code}</pre>
-        </div>
-    )
-}
-ReactDOM.render(<App />, document.querySelector('#root'))
+      <button onClick={onClick}>Submit</button>
+      <pre>{code}</pre>
+      <iframe
+        title="preview"
+        ref={iframe}
+        sandbox="allow-scripts"
+        srcDoc={html}
+      />
+    </div>
+  );
+};
+ReactDOM.render(<App />, document.querySelector("#root"));
