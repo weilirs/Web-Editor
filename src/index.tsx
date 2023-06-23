882430f1360442e50aa247e9c32adbcc6c6564a9
weilirs <weilirs@umich.edu>
2023-06-23 09:45:37 -0500

    added editor

--- src/index.tsx
+++ src/index.tsx
@@ -1,84 +1,16 @@
-import * as esbuild from "esbuild-wasm";
-import ReactDOM from "react-dom";
-import { useState, useEffect, useRef } from "react";
-import { unpkgPlugin } from "./plugins/unpkg-plugin";
-import { fetchPlugin } from "./plugins/fetch-plugin";
+import * as esbuild from "esbuild-wasm"
+import ReactDOM from "react-dom"
+import { useState, useEffect, useRef } from "react"
+import { unpkgPlugin } from "./bundler/plugins/unpkg-plugin"
+import { fetchPlugin } from "./bundler/plugins/fetch-plugin"
+import CodeEditor from "./components/code-editor"
+import CodeCell from "./components/code-cell"
 
 const App = () => {
-  const ref = useRef<any>();
-  const iframe = useRef<any>();
-  const [input, setInput] = useState("");
-  const [code, setCode] = useState("");
-
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
-    });
-  };
-
-  useEffect(() => {
-    startService();
-  }, []);
-
-  const onClick = async () => {
-    if (!ref.current) {
-      return;
-    }
-
-    iframe.current.srcdoc = html;
-
-    const result = await ref.current.build({
-      entryPoints: ["index.js"],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPlugin(), fetchPlugin(input)],
-      define: {
-        process_env_NODE_ENV: '"production"',
-        global: "window",
-      },
-    });
-
-    // console.log(`JS CODE: ${result.outputFiles[0].text}`);
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
-  };
-
-  const html = `
-    <html>
-        <head></head>
-        <body>
-            <div id="root"></div>
-            <script>
-                window.addEventListener('message', (event) => {
-                    try {
-                        eval(event.data)
-                    } catch (err) {
-                        const root = document.querySelector('#root')
-                        root.innerHTML = '<div style="color: red;"><h4> Runtime Error </h4>' + err + '</div>';
-                        console.log(err)
-                    }
-                },false)
-            </script>
-        </body>
-    </html>
-  `;
-
   return (
     <div>
-      <textarea
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      ></textarea>
-
-      <button onClick={onClick}>Submit</button>
-      <pre>{code}</pre>
-      <iframe
-        title="preview"
-        ref={iframe}
-        sandbox="allow-scripts"
-        srcDoc={html}
-      />
+      <CodeCell />
     </div>
-  );
-};
-ReactDOM.render(<App />, document.querySelector("#root"));
+  )
+}
+ReactDOM.render(<App />, document.querySelector("#root"))
