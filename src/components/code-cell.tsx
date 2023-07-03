44118dd398928e1b1ce3203291855276b349dc10
weilirs <weilirs@umich.edu>
2023-07-03 12:58:52 -0500

    feat: Cumulative code execution

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -7,6 +7,7 @@ import React, { useEffect, useState } from "react"
 import { Cell } from "../state"
 import { useActions } from "../hooks/use-actions"
 import { useTypedSelector } from "../hooks/use-typed-selector"
+import { useCumulativeCode } from "../hooks/use-cumulative-code"
 
 interface CodeCellProps {
   cell: Cell
@@ -15,19 +16,20 @@ interface CodeCellProps {
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const { updateCell, createBundle } = useActions()
   const bundle = useTypedSelector((state) => state.bundles[cell.id])
+  const cumulativeCode = useCumulativeCode(cell.id)
   useEffect(() => {
     if (!bundle) {
-      createBundle(cell.id, cell.content)
+      createBundle(cell.id, cumulativeCode)
       return
     }
     const timer = setTimeout(async () => {
-      createBundle(cell.id, cell.content)
+      createBundle(cell.id, cumulativeCode)
     }, 750)
 
     return () => {
       clearTimeout(timer)
     }
-  }, [cell.content, cell.id, createBundle])
+  }, [cumulativeCode, cell.id, createBundle])
 
   return (
     <Resizable direction="vertical">
@@ -40,7 +42,7 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
       >
         <Resizable direction="horizontal">
           <CodeEditor
-            initialValue="const a = 1;"
+            initialValue={cell.content}
             onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
